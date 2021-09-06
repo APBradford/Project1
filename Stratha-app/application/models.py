c377ff3baa30e183d828ba3fc073144ab87b212a
Alex Bradford <alexpbradford@gmail.com>
2021-09-06 19:32:49 +0000

    Fixed table relationships

--- Stratha-app/application/models.py
+++ Stratha-app/application/models.py
@@ -3,13 +3,13 @@ from application import db
 class Training_week(db.Model):
     __tablename__ = "training"
     training_week_id = db.Column(db.Integer, primary_key = True)
-    cycles = db.relationship('cycling', backref = 'training-week')
+    cycles = db.relationship('Cycling', backref='training-week', lazy=True)
     cycle_distance = db.Column(db.Float, nullable = False)
 
 class Cycling(db.Model):
     __tablename__ = "cycling"
-    cycle_id = db.Column(db.Integer, primary_key = True)
-    training_week = db.Column(db.Integer, nullable = False)
-    date = db.Column(db.DateTime, nullable = False)
-    distance = db.Column(db.Float, db.ForeignKey('training.training_week_id'), nullable = False)
+    cycle_id = db.Column(db.Integer,  primary_key = True)
+    training_week = db.Column(db.Integer, db.ForeignKey('training.training_week_id'), nullable = False)
+    date = db.Column(db.String(10), nullable = False)
+    distance = db.Column(db.Float, nullable = False)
     comment = db.Column(db.String(250), nullable = False)
